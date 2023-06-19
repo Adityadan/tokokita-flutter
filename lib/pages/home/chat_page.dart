5100fe05f423dab838cc770530da98fefe01c0ee
Aditya Dani <danirama282@gmail.com>
2023-06-19 13:36:16 +0700

    upload project flutter tokokita

---
+++ lib/pages/home/chat_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class chatPage extends StatelessWidget {
+  const chatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text('chat Page')),
+    );
+  }
+}
---
+++ lib/pages/home/profile_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class profileDart extends StatelessWidget {
+  const profileDart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text('Profile Page')),
+    );
+  }
+}
